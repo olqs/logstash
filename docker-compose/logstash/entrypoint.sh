73237ee32ba35200f3ea102b7fb59fd10dd38d39
Alexander Worlitschek <github@olqs.de>
2024-10-02 13:56:14 +0200

    using latest release version instead of fixed version number

--- docker-compose/logstash/entrypoint.sh
+++ docker-compose/logstash/entrypoint.sh
@@ -1,8 +1,10 @@
-#!/bin/bash
+#!/bin/bash -x
 
 # Download depot_id to steam_game_id and name mapping
 
-curl -L -o /usr/share/logstash/config/app-depot-output-cleaned.csv https://github.com/devedse/DeveLanCacheUI_SteamDepotFinder_Runner/releases/download/1.0.88/app-depot-output-cleaned.csv
+LATEST_VERSION=`curl -H "Accept: application/json" -L https://github.com/devedse/DeveLanCacheUI_SteamDepotFinder_Runner/releases/latest | sed 's/.*"tag_name":"\([^"]*\).*/\1/'`
+
+curl -L -o /usr/share/logstash/config/app-depot-output-cleaned.csv "https://github.com/devedse/DeveLanCacheUI_SteamDepotFinder_Runner/releases/download/${LATEST_VERSION}/app-depot-output-cleaned.csv"
 
 # Split combined list to two separate lists depot_id -> game_id and depot_id -> game_name
 awk -F\; '$3 !~ /denied/ { print $3","$1 }' /usr/share/logstash/config/app-depot-output-cleaned.csv > /usr/share/logstash/config/depotid-steamid.csv
